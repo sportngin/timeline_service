148659ae7d235f93b2672d616269591b15237d28
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-03-17 17:39:52 -0500

    add some filters

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,19 @@
 class Entry < ActiveRecord::Base
+  # Public: Filter results by the given user_id
+  #
+  # user_id - ID of the user to search for
+  #
+  # Returns a Relation
+  def self.by_user(user_id)
+    where(user_id: user_id)
+  end
 
+  # Public: Filter entries by the given action type
+  #
+  # action_type - Key identifying the action to search for
+  #
+  # Returns a Relation
+  def self.by_action(action_type)
+    where(action_type: action_type)
+  end
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,5 +1,31 @@
 require "spec_helper"
 
 describe Entry do
+  context "#valid?" do
+    pending
+  end
 
+  context ".user(user_id)" do
+    let!(:user1) { FactoryGirl.create(:entry, user_id: 1) }
+    let!(:user2) { FactoryGirl.create(:entry, user_id: 2) }
+
+    subject { Entry.by_user(2) }
+
+    it "returns only entries with the given user_id" do
+      expect(subject.count).to eq(1)
+      expect(subject.load).to include user2
+    end
+  end
+
+  context ".by_action(action_type)" do
+    let!(:log_in) { FactoryGirl.create(:entry, action_type: "log-in") }
+    let!(:create_page) { FactoryGirl.create(:entry, action_type: "page-create") }
+
+    subject { Entry.by_action("log-in") }
+
+    it "returns only entries with the given action" do
+      expect(subject.count).to eq(1)
+      expect(subject.load).to include log_in
+    end
+  end
 end
