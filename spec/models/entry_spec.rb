b396a4ea1901bbd464715752b27875c3d7559e52
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-03-17 17:49:58 -0500

    remove now-redundant specs

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -24,28 +24,4 @@ describe Entry do
       expect(entries.load).to include log_in
     end
   end
-
-  context ".user(user_id)" do
-    let!(:user1) { FactoryGirl.create(:entry, user_id: 1) }
-    let!(:user2) { FactoryGirl.create(:entry, user_id: 2) }
-
-    subject { Entry.user(2) }
-
-    it "returns only entries with the given user_id" do
-      expect(subject.count).to eq(1)
-      expect(subject.load).to include user2
-    end
-  end
-
-  context ".action(action_type)" do
-    let!(:log_in) { FactoryGirl.create(:entry, action_type: "log-in") }
-    let!(:create_page) { FactoryGirl.create(:entry, action_type: "page-create") }
-
-    subject { Entry.action("log-in") }
-
-    it "returns only entries with the given action" do
-      expect(subject.count).to eq(1)
-      expect(subject.load).to include log_in
-    end
-  end
 end
