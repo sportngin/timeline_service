d66d3c9ceb92d6a5623deddc737aae17edbda994
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-03-17 21:12:50 -0500

    mark some columns as not nullable

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130318020604) do
+ActiveRecord::Schema.define(version: 20130318020734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "entries", force: true do |t|
-    t.integer  "user_id"
+    t.integer  "user_id",     null: false
     t.string   "user_name"
-    t.string   "action_type"
-    t.datetime "happened_at"
-    t.json     "metadata"
+    t.string   "action_type", null: false
+    t.datetime "happened_at", null: false
+    t.json     "metadata",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
