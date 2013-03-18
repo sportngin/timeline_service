de0d96617bc9e7a6cba79e877707b07aa817714a
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-03-18 05:33:10 -0500

    install haml dependency

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'turbolinks'
 
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.0.1'
+gem 'haml-rails'
 
 group :test, :development do
   gem 'rspec-rails'
--- spec/controllers/entries_controller_spec.rb
+++ spec/controllers/entries_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe EntriesController do
+  context "#index" do
+    let(:entry) { FactoryGirl.build(:entry) }
+    let(:params) { {search: {user: 123}} }
 
+    it "filters on the given search criteria" do
+      Entry.should_receive(:periscope).with(params[:search]) { [entry] }
+      get :index, params
+      expect(assigns[:entries]).to eq([entry])
+    end
+
+    it "responds to HTML requests"
+
+    it "responds to JSON requests"
+
+    it "paginates results"
+  end
 end
