6169dedac20f1d598445a21c316d5d5d4e331df4
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-03-17 21:07:21 -0500

    add indexes to the searchable columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130317182348) do
+ActiveRecord::Schema.define(version: 20130318020604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,5 +25,9 @@ ActiveRecord::Schema.define(version: 20130317182348) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "entries", ["action_type"], name: "index_entries_on_action_type"
+  add_index "entries", ["happened_at"], name: "index_entries_on_happened_at"
+  add_index "entries", ["user_id"], name: "index_entries_on_user_id"
 
 end
